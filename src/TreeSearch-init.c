826107e8da21fb8232c65820e276b285a4ab1c14
Martin R. Smith <martins@gmail.com>
2017-07-27 06:54:33 +0100

    Make ape/phangorn imports clearer
    Attempts to import using useDynLib failed.

--- src/TreeSearch-init.c
+++ src/TreeSearch-init.c
@@ -6,10 +6,23 @@
 #include <Rinternals.h>
 
 #include "phangorn_fitch.h"
-#include "reorder.h"
+#include "ape_reorder.h"
+#include "renumber_tree.h"
+
+// Abandoned: attempts to link to ape / phangorn functions
+// static R_NativePrimitiveArgType ape_node_depth_t[] = {
+//   INTSXP, INTSXP, INTSXP, INTSXP, INTSXP, REALSXP, INTSXP
+// };
+// void ape_node_depth(int *ntip, int *nnode, int *e1, int *e2,
+// 		int *nedge, double *xx, int *method) {
+//   typedef int (*Fun)(int*, int*, int*, int*, int*, double*, int*);
+//   Fun fun = (Fun) R_GetCCallable( "ape", "node_depth" );   
+//   fun(ntip, nnode, e1, e2, nedge, xx, method);
+// }
 
 static const R_CMethodDef cMethods[] = {
   {"order_edges_number_nodes", (DL_FUNC) &order_edges_number_nodes, 4, order_edges_number_nodes_t},
+  {"ape_node_depth", (DL_FUNC) &ape_node_depth, 7, ape_node_depth_t},
   {"ape_neworder_phylo", (DL_FUNC) &ape_neworder_phylo, 6, ape_neworder_phylo_t},
   {"ape_neworder_pruningwise", (DL_FUNC) &ape_neworder_pruningwise, 6, ape_neworder_pruningwise_t},
   {NULL, NULL, 0, NULL}
