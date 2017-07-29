dbd8941021a16376d6edf106c7068f2bdb357c0f
Martin R. Smith <martins@gmail.com>
2017-07-29 11:07:02 +0100

    Register order-edges_number-nodes as callable

--- src/TreeSearch-init.c
+++ src/TreeSearch-init.c
@@ -38,7 +38,7 @@ static const R_CallMethodDef callMethods[] = {
 
 void R_init_TreeSearch(DllInfo *info) {
   R_registerRoutines(info, cMethods, callMethods, NULL, NULL);
-  R_RegisterCCallable("TreeSearch", "order_edges_number_nodes", order_edges_number_nodes);
+  R_RegisterCCallable("TreeSearch", "order_edges_number_nodes", (DL_FUNC) &order_edges_number_nodes);
   R_useDynamicSymbols(info, FALSE);
   R_forceSymbols(info, TRUE);
 }
