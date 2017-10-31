d9668a13ebc7ca4f55ded247e4c849f035388b76
Martin R. Smith <martins@gmail.com>
2017-10-31 08:51:10 +0000

    * DELETE unused function Min

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,12 +1,3 @@
-
-#' @keywords internal
-#' @export
-Min <- function (x, inappLevel) {
-  if (length(inappLevel)) return(sum(2^(c(0:(inappLevel-2), inappLevel:12)) %in% unique(x)))
-  return (sum(2^(0:12) %in% unique(x)))
-}
-
-
 #' Quick sample
 #' 
 #' Faster than inbuilt sample because it avoids some checks
