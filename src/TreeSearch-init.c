8986931f41e42772e41c2582979a4ced997e167c
Martin R. Smith <martins@gmail.com>
2017-10-31 11:24:12 +0000

    Spacing

--- src/TreeSearch-init.c
+++ src/TreeSearch-init.c
@@ -21,26 +21,24 @@
 //   fun(ntip, nnode, e1, e2, nedge, xx, method);
 // }
 
-
-
 static const R_CMethodDef cMethods[] = {
   {"order_edges_number_nodes", (DL_FUNC) &order_edges_number_nodes, 3, order_edges_number_nodes_t},
-  {"ape_neworder_phylo", (DL_FUNC) &ape_neworder_phylo, 6, ape_neworder_phylo_t},
-  {"ape_node_depth", (DL_FUNC) &ape_node_depth, 7, ape_node_depth_t},
+  {"ape_neworder_phylo",       (DL_FUNC) &ape_neworder_phylo, 6, ape_neworder_phylo_t},
+  {"ape_node_depth",           (DL_FUNC) &ape_node_depth, 7, ape_node_depth_t},
   {"ape_neworder_pruningwise", (DL_FUNC) &ape_neworder_pruningwise, 6, ape_neworder_pruningwise_t},
   {NULL, NULL, 0, NULL}
 };
 
 static const R_CallMethodDef callMethods[] = {
   {"phangorn_FITCH", (DL_FUNC) &phangorn_FITCH, 8},
-  {"RENUMBER_TREE", (DL_FUNC) &RENUMBER_TREE, 3},
+  {"RENUMBER_TREE",  (DL_FUNC) &RENUMBER_TREE,  3},
   {NULL, NULL, 0}
 };
 
-void R_init_TreeSearch(DllInfo *info) {
-  R_registerRoutines(info, cMethods, callMethods, NULL, NULL);
+void R_init_TreeSearch(DllInfo *dll) {
+  R_registerRoutines(dll, cMethods, callMethods, NULL, NULL);
   R_RegisterCCallable("TreeSearch", "RENUMBER_TREE", (DL_FUNC) &RENUMBER_TREE);
-  R_useDynamicSymbols(info, FALSE);
-  R_forceSymbols(info, TRUE);
+  R_useDynamicSymbols(dll, FALSE);
+  //R_forceSymbols(dll, TRUE);
 }
 
