3eed9425ecf2782bd6264c94178b03f6ed3c4d52
Martin R. Smith <martins@gmail.com>
2017-11-04 18:21:13 +0000

    Adapt RootedNNI to deliver all trees

--- R/NNI.R
+++ R/NNI.R
@@ -10,7 +10,8 @@
 #' @template treeParam
 #' @template edgeToBreakParam
 #' 
-#' @return Returns a tree with class \code{phylo}.
+#' @return Returns a tree with class \code{phylo} (if \code{returnAll = FALSE}) or 
+#'         a set of trees, with class \code{multiPhylo} (if \code{returnAll = TRUE}).
 #'
 #' @references
 #' The algorithm is summarized in
@@ -19,19 +20,27 @@
 #' @author Martin R. Smith
 #' 
 #' @examples
-#' tree <- ape:::rtree(20, br=NULL)
+#' tree <- ape::rtree(20, br=NULL)
 #' NNI(tree)
+#' NNI(tree, all=TRUE)
 #'
 #' @export
 NNI <- function (tree, edgeToBreak=NULL) {
   edge    <- tree$edge
   parent  <- edge[, 1]
   child   <- edge[, 2]
-  nTips  <- (length(parent) / 2L) + 1L
+  nEdge   <- length(parent)
+  nTips   <- (nEdge / 2L) + 1L
   rootNode <- nTips + 1L
   
-  samplable <- child > nTips
-  if (is.null(edgeToBreak)) { 
+  if (!any(samplable)) stop("Not enough edges to allow NNI rearrangement")
+  
+  if (edgeToBreak == -1) {
+    # newEdges <- vapply(which(samplable), DoubleNNI, parent=parent, child=child, list(matrix(0L, nEdge, 2), matrix(0L, nEdge, 2)))
+    newEdges <- unlist(lapply(which(samplable), DoubleNNI, parent=parent, child=child), recursive=FALSE) # Quicker than vapply, surprisingly
+    newTrees <- lapply(newEdges, function (edges) {tree$edge <- edges; tree}) # Quicker than vapply, surprisingly
+    return(newTrees)
+  } else if (is.null(edgeToBreak)) { 
     edgeToBreak <- SampleOne(which(samplable))
   } else {
     if (!samplable[edgeToBreak]) stop("edgeToBreak must be an internal edge")
@@ -50,7 +59,45 @@ NNI <- function (tree, edgeToBreak=NULL) {
   child[oldInd] <- childSwap
   
   tree$edge <- RenumberTree(parent, child)
-  tree
+  return(tree)
+  }
+}
+
+#' Double NNI
+#' 
+#' Returns the edge parameter of the two trees consistent with the speficied \acronym{NNI} rearrangement
+#'
+#' @param parent tree$edge[, 1]
+#' @param child  tree$edge[, 2]
+#' @param edgeToBreak index of edge to break
+#'
+#' @return the \code{tree$edge} parameter of the two trees consistent with the specified rearrangement
+#'
+#' @keywords internal
+#' @author Martin R. Smith
+#'
+DoubleNNI <- function (parent, child, edgeToBreak) {
+  end1   <- parent[edgeToBreak]
+  end2   <- child[edgeToBreak]
+  ind1   <- which(parent == end1)
+  ind1   <- ind1[ind1 != edgeToBreak][1]
+  ind2.3 <- which(parent == end2)
+  ind2   <- ind2.3[1]
+  ind3   <- ind2.3[2]
+
+  newInd <- c(ind2, ind1)
+  oldInd <- c(ind1, ind2)
+  child2 <- child
+  childSwap <- child[newInd]
+  child2[oldInd] <- childSwap
+  
+  newInd <- c(ind3, ind1)
+  oldInd <- c(ind1, ind3)
+  childSwap <- child[newInd]
+  child[oldInd] <- childSwap
+  
+  nEdge <- length(parent)
+  return(list(RenumberTree(parent, child, nEdge), RenumberTree(parent, child2, nEdge)))
 }
 
 #' Rooted NNI 
@@ -64,7 +111,11 @@ RootedNNI <- function (tree, edgeToBreak = NULL) {
   rootNode <- nTips + 1L
   
   samplable <- parent != rootNode & child > nTips
-  if (is.null(edgeToBreak)) { 
+  if (edgeToBreak == -1) {
+    newEdges <- unlist(lapply(which(samplable), DoubleNNI, parent=parent, child=child), recursive=FALSE) # Quicker than vapply, surprisingly
+    newTrees <- lapply(newEdges, function (edges) {tree$edge <- edges; tree}) # Quicker than vapply, surprisingly
+    return(newTrees)   
+  } else if (is.null(edgeToBreak)) { 
     edgeToBreak <- SampleOne(which(samplable))
   } else {
     if (!samplable[edgeToBreak]) stop("edgeToBreak cannot include a tip or the root node")
--- man-roxygen/edgeToBreakParam.R
+++ man-roxygen/edgeToBreakParam.R
@@ -1,1 +1,3 @@
-#' @param edgeToBreak (optional) integer specifying the index of an edge to bisect/prune, generated randomly if not specified
+#' @param edgeToBreak (optional) integer specifying the index of an edge to bisect/prune,
+#' generated randomly if not specified.  Alternatively, set to \code{-1} to return a complete list
+#' of all trees one step from the input tree.
