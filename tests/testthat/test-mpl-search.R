ec820ddea0eb3fff35044303e82e28d4b9320fdf
Martin R. Smith <martins@gmail.com>
2017-11-10 21:07:51 +0000

    Revise functions for new hyperefficient approach

--- tests/testthat/test-mpl-search.R
+++ tests/testthat/test-mpl-search.R
@@ -8,13 +8,20 @@ test_that("tree search finds shortest tree", {
   malformed_tree <- ape::read.tree(text = "((((1,2),3),4),5,6);")
   dataset <- StringToPhyDat('110000 111000 111100', 1:6, byTaxon=FALSE)
   expect_error(TreeSearch(malformed_tree, dataset))
-  start_tree <- TreeSearch::RenumberTips(read.tree(text = "(((1, 6), 3), (2, (4, 5)));"), true_tree$tip.label)
+  start_tree <- RenumberTips(read.tree(text = "(((1, 6), 3), (2, (4, 5)));"), true_tree$tip.label)
   expect_equal(InapplicableFitch(start_tree, dataset), 6)
-  expect_equal(InapplicableFitch(BasicSearch(start_tree, dataset, Rearrange=NNICore, verbosity=0), dataset),
-               InapplicableFitch(true_tree, dataset), 3)
-  expect_equal(InapplicableFitch(BasicSearch(start_tree, dataset, Rearrange=SPRCore, verbosity=0), dataset),
-               InapplicableFitch(true_tree, dataset), 3)
-  expect_equal(InapplicableFitch(BasicSearch(start_tree, dataset, Rearrange=TBRCore, verbosity=0), dataset),
-               InapplicableFitch(true_tree, dataset), 3)
-  expect_equal(InapplicableFitch(RatchetSearch(start_tree, dataset, rearrangements=list(TreeSearch::TBR, TreeSearch::SPR, TreeSearch::NNI), k=3, maxHits=5, verbosity=0), dataset), InapplicableFitch(true_tree, dataset), 3)
+  morphyObj <- PhyDat2Morphy(dataset)
+  on.exit(morphyObj <- UnloadMorphy(morphyObj))
+  
+ ## TODO FIX VERBOISTY
+  expect_equal(3, BasicSearch(start_tree, dataset, Rearrange=NNICore, verbosity=-1)[[3]],
+               InapplicableFitch(true_tree, dataset))
+  expect_equal(3, BasicSearch(start_tree, dataset, Rearrange=SPRCore, verbosity=-1)[[3]],
+               InapplicableFitch(true_tree, dataset))
+  expect_equal(3, BasicSearch(start_tree, dataset, Rearrange=TBRCore, verbosity=-1)[[3]],
+               InapplicableFitch(true_tree, dataset))
+  ratchetScore <- RatchetSearch(start_tree, dataset, 
+                  rearrangements=list(TreeSearch::TBR, TreeSearch::SPR, TreeSearch::NNI),
+                  k=3, maxHits=5, verbosity=0)[[3]]
+  expect_equal(3, InapplicableFitch(true_tree, dataset), ratchetScore)
 })
