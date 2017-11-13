8dc3aad40af4f3431f233de34b57e58b35befdb8
Martin R. Smith <martins@gmail.com>
2017-11-13 16:10:17 +0000

    No warnings required

--- tests/testthat/test-pp-ICS.R
+++ tests/testthat/test-pp-ICS.R
@@ -1,7 +1,7 @@
 context("Profile Parsimony: Information content of steps")
 
 test_that("Information content of steps calculated correctly", {
-  expect_equal(c(1, 2), as.double(ICS(2, 2, 10000) * NUnrooted(4)))
+  expect_equal(c(1, 2), as.double(ICS(2, 2, 10000, warn=FALSE) * NUnrooted(4)))
   expect_equal(c(3, 12), signif(as.double(ICS(2, 3, 10000, warn=FALSE) * NUnrooted(5), 5)))
   
   expect_true(max(abs(cumsum(as.double(ICS(3,3, 6000, warn=FALSE) * NUnrooted(6))) - 
