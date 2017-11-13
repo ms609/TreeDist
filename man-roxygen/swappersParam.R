a11dd214caed4b801c0263c0a8ac201489d3d11c
Martin R. Smith <martins@gmail.com>
2017-11-13 17:54:49 +0000

    Rearrange -> EdgeSwapper

--- man-roxygen/swappersParam.R
+++ man-roxygen/swappersParam.R
@@ -1,3 +1,6 @@
-#' @param swappers A list of functions to use to conduct edge rearrangement.
+#' @param swappers A list of functions to use to conduct edge rearrangement during tree search.
 #'                 Provide functions like \code{\link{NNISwap}} to shuffle root position,
 #'                 or \code{\link{RootedTBRSwap}} if the position of the root should be retained.
+#'                 You may wish to use extreme swappers (such as \acronym{TBR}) early in the list,
+#'                 and a more subtle rearranger (such as \acronym{NNI}) later in the list to make
+#'                 incremental tinkerings once an almost-optimal tree has been found.
