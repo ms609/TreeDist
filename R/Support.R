f2b5889a0065c764b00ff12952a9e16d90996782
Martin Smith <martins@gmail.com>
2019-02-13 09:20:05 +0000

    Tree2Splits in TreeSearch, not Quartet

--- R/Support.R
+++ R/Support.R
@@ -8,9 +8,9 @@
 #' 
 #' 
 #' @param reference A tree of class phylo, or a character vector specifying its splits 
-#'                     (as obtained through [Quartet::Tree2Splits])
+#'                     (as obtained through [Tree2Splits])
 #' @param forest a list of trees of class phylo, or a multiPhylo object; or a
-#' list of their constituent splits (as obtained through [Quartet::Tree2Splits])
+#' list of their constituent splits (as obtained through [Tree2Splits])
 #'                     
 #' @return Number of trees in `forest` that contain each split in `reference`. 
 #'         if `reference` is a tree of class phylo, then the sequence will
@@ -64,7 +64,7 @@ SplitFrequency <- function(reference, forest) {
 #' `matches`.
 #' 
 #' @param original,matches Logical matrix describing the bipartition splits
-#' defined by a tree (see [Quartet:Tree2Splits] for format).  The first row
+#' defined by a tree (see [Tree2Splits] for format).  The first row
 #' of `original` must be `FALSE` for all splits.
 #' 
 #' @keywords internal
