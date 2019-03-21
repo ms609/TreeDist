7e46fdb8cec2e7e11b4218e97e25c30df10f75b7
Martin R. Smith <martins@gmail.com>
2019-03-21 11:17:44 +0000

    Add some tests (#46)
    * Import information functions from `infopars`
    
    * Reduce code for 2 tiles, 2 colours
    
    * Simplify code
    
    Now passing tests, too (-8
    
    * Legal test for setdiff
    
    * Use DoubleFactorial in place of DFact
    
    * seq_len, not :
    
    * typo
    
    * @concept-s-
    
    * Remove memoization & document
    
    * Cache double factorials
    
    * Optimize NUnrooted
    
    * Optimise for speed
    
    * Simplify ConsistentWithBoth
    
    * Optimise code
    
    * Use mapply
    
    * Compare lists of trees
    
    * Test tree-to-list comparisons
    
    * Define variables in global scope
    
    * Definition of doubleFactorial variables
    
    * Export doubleFactorial
    
    * Try .data$doubleFactorial
    
    * Cache using .data
    
    * document()
    
    * Use utils::globalVariables()
    
    * mapply dimensions
    
    * Quicker split name checking
    
    * ncol/row -> dim
    
    * Optimize
    
    * any in place of all
    
    * Extra optimization
    
    * Documentation errors
    
    * sp
    
    * File structure
    
    * Improve test coverage
    
    * Test coverage
    
    * Test and improve N1Spr
    
    * typo
    
    * Update N1Spr.Rd
    
    * Correct test
    
    * Format
    
    * Format
    
    * Format
    
    * Joint information tests
    
    * Joint information of contradictory splits
    
    * document()
    
    * Probability of splits matching by chance
    
    * Use entropy to determine how good a split pairing is
    
    * Entropy-based combinatorics
    
    * Calculate split matching probability with entropy match score criterion
    
    * Can't use .Last.value
    
    * Better calculation of match quality
    
    * SplitMatchProbability
    
    * document()
    
    * Rename MutualInfo
    
    * Documentation
    
    * reportMatching
    
    * Calculate dimensions once
    
    * Fix arrangements
    
    * NPartitionPairs
    
    * Doc
    
    * reportMatching
    
    * Document splits1, splits2
    
    * Document
    
    * Variation of Info
    
    From Melia 2007
    
    * Document `MinimumSteps`
    
    * Document and expand SplitEntropy
    
    * Progress with counting all split pairings
    
    * Test AllSplitPairings
    
    * Entropy
    
    * SplitMUtualInformation
    
    * AllSplitPairings (preliminary)
    
    * latex syntax
    
    * New functions for partitionwise comparisons
    
    Will need re-naming, and possibly refining.
    
    * Variation of Information ideas
    
    * Update test-information.R
    
    * Update test-tree-distance.R
    
    * Document
    
    * Prepare for new RNG in R3.6.0
    
    suppressWarnings(RNGversion("3.5.0")) # Until we can require R3.6.0
    
    * rn to MutualSplitClusteringInfo
    
    * Start documenting properly
    
    * Tidy code
    
    * Document
    
    * Avoid duplicate params
    
    * Deprecate TreeSplits
    
    * Require memoize
    
    * Update NEWS.md
    
    * suppressWarnings(RNGversion("3.5.0")) # Until we can require R3.6.0
    
    * suppressWarnings(RNGversion("3.5.0")) # Until we can require R3.6.0
    
    * Improve documentation
    
    * test_that("Entropy is calculated correctly",
    
    * Documentation
    
    * -MeilaVariationOfInformation
    
    * Delete AnySizeSplitMatchProbability.Rd
    
    * Documentation
    
    * Documentation
    
    * Spelling
    
    * Documentation
    
    * Use markdown in documentation
    
    * Improve documentation
    
    * Documentation format for markdown compatability
    
    * * REMOVE FullMutualInformation
    
    * document TreesConsistentWithTwoSplits in SplitMutualInformation
    
    * First pass at documenting `Information.R`
    
    * Support unnamed splits
    
    * test_that("Removing contradictions improves scores",
    
    * Remove `FullMutualInformation` tests
    
    * Update SplitsCompatible.Rd
    
    * Balance braces
    
    * Documentation and naming consistency
    
    * Remove MutualPartitionInfo relic
    
    * Rename and re-order
    
    * Documentation; start preparing for release()
    
    * Prepare for v0.3.0 release
    
    Remove comments to pass `check`
    
    * Minor efficiency gains (?)
    
    * Fix link
    
    * Update cran-comments.md
    
    * Release v0.3.0
    
    * + tests
    
    * Add tests
    
    * Use helper function to call tree distance calculators
    
    * Typos
    
    * Nye et al. (2006) similarity measure

--- tests/testthat/test-tree-distance.R
+++ tests/testthat/test-tree-distance.R
@@ -13,6 +13,7 @@ test_that('Tree differences are correctly calculated', {
   treeSym8 <- ape::read.tree(text='((e, (f, (g, h))), (((a, b), c), d));')
   treeBal8 <- ape::read.tree(text='(((e, f), (g, h)), ((a, b), (c, d)));')
   treeOpp8 <- ape::read.tree(text='(((a, f), (c, h)), ((g, b), (e, d)));')
+  treeBadLabel8 <- ape::read.tree(text='((a, b, c, D), (e, f, g, h));')
   
   treeCat8 <- ape::read.tree(text='((((h, g), f), e), (d, (c, (b, a))));')
   treeTac8 <- ape::read.tree(text='((((e, c), g), a), (h, (b, (d, f))));')
@@ -23,11 +24,26 @@ test_that('Tree differences are correctly calculated', {
   treeTwoSplits <- ape::read.tree(text="(((a, b), c, d), (e, f, g, h));")
 
   # Labels differ
-  expect_error(MutualArborealInfo(treeSym8, ape::read.tree(text='((a, b, c, D), (e, f, g, h));')))
+  expect_error(MutualArborealInfo(treeSym8, treeBadLabel8))
+  expect_error(VariationOfArborealInfo(treeSym8, treeBadLabel8))
+  expect_error(MutualClusterInfo(treeSym8, treeBadLabel8))
+  expect_error(NyeTreeDistance(treeSym8, treeBadLabel8))
+
   expect_equal(22.53747, round(MutualArborealInfo(treeSym8, treeSym8), 5))
   expect_equal(13.75284, round(MutualArborealInfo(treeSym8, treeBal8), 5))
   expect_equal(-log2(945/10395), MutualArborealInfo(treeSym8, treeAb.Cdefgh))
   expect_equal(-log2(315/10395), MutualArborealInfo(treeSym8, treeAbc.Defgh))
+  
+  expect_equal(5L, NyeTreeSimilarity(treeSym8, treeSym8))
+  expect_equal(2, 3 * NyeTreeSimilarity(treeAb.Cdefgh, treeAbc.Defgh))
+  expect_equal(3.8, NyeTreeSimilarity(treeSym8, treeBal8))
+  expect_true(NyeTreeSimilarity(treeSym8, treeBal8) > NyeTreeSimilarity(treeSym8, treeOpp8))
+  
+  # TODO: These are only superficial tests of these functions.
+  # Write tests based on calculated values.
+  expect_equal(0, VariationOfArborealInfo(treeSym8, treeSym8))
+  expect_equal(MutualClusterInfo(treeSym8, treeSym8), 
+               MutualClusterInfo(treeBal8, treeBal8))
   
   # Test symmetry of small vs large splits
   expect_equal(MutualArborealInfo(treeSym8, treeAbc.Defgh),
