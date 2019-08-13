5ac2dafe31577720fe066ecc38ed1645d94668cd
Martin Smith <martins@gmail.com>
2019-08-13 17:03:06 +0100

    Standalone TreeDistPlot

--- R/plot.R
+++ R/plot.R
@@ -12,27 +12,32 @@
 #' @param \dots Additional parameters to `plot.phylo`
 #'
 #' @author Martin R. Smith
+#' @importFrom ape plot.phylo
 #' @keywords internal
 #' @export
 TreeDistPlot <- function (tr, title=NULL, bold=NULL, leaveRoom = TRUE,
                      prune=integer(0), graft=integer(0), ...) {
+  
   if (is.null(tr$edge.length)) tr$edge.length <- rep(1, dim(tr$edge)[1])
   if (is.null(tr$edge.width)) tr$edge.width <- rep(1, dim(tr$edge)[1])
   edge.color <- rep('black', dim(tr$edge)[1])
 
   if (length(prune) > 0 || length(graft) > 0) {
     tr$edge.width[c(prune, graft)] <- 3L
-    edge.color[prune] <- cbPalette8[7]
-    edge.color[graft] <- cbPalette8[4]
+    edge.color[prune] <- "#D55E00" # Ternary::cbPalette8[7]
+    edge.color[graft] <- "#009E73" # Ternary::cbPalette8[4]
   }
+  tipCols <- c("#000000", "#004949", "#009292", "#FFB6DB", "#490092", "#B66DFF", 
+               "#6DB6FF", "#B6DBFF", "#920000", "#924900", "#DB6D00", "#24FF24", 
+               "#FFFF6D") # Ternary::cbPalette15[-c(4, 7)]
 
   tipNumbers <- tr$tip.label
   font <- rep(1L, length(tipNumbers))
   if (!is.null(bold)) font[tipNumbers %in% bold] <- 4L
-  yLim <- if (leaveRoom) c(-0.4 - length(legendSequence), 11) else c(-0.4, 11)
+  yLim <- if (leaveRoom) c(-0.4 - 8 # = -0.4 - length(legendSequence)
+                           , 11) else c(-0.4, 11)
   tr$tip.label <- LETTERS[as.integer(tipNumbers)]
-  plot(tr, tip.col=Ternary::cbPalette15[-c(4, 7)][as.integer(tipNumbers)],
-       main=title, cex.main=0.8, font=font,
-       edge.width=tr$edge.width, edge.color=edge.color,
-       y.lim=yLim, ...)
+  plot(tr, tip.col=tipCols[as.integer(tipNumbers)], main=title, cex.main=0.8, 
+       font=font, edge.width=tr$edge.width, edge.color=edge.color, y.lim=yLim, 
+       ...)
 }
