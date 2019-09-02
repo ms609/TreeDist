0acbc543aa0d931c28700ee3d1a56ba365913fc7
Martin Smith <martins@gmail.com>
2019-09-02 09:44:55 +0100

    Convert to `GeneralizedRF`

--- R/tree_distance_msd.R
+++ R/tree_distance_msd.R
@@ -36,65 +36,27 @@ MatchingSplitDistance <- function (tree1, tree2, normalize = FALSE,
 #' @export
 MatchingSplitDistanceSplits <- function (splits1, splits2, normalize = TRUE, 
                                          reportMatching = FALSE) {
-  
-  dimSplits1 <- dim(splits1)
-  dimSplits2 <- dim(splits2)
-  nTerminals <- dimSplits1[1]
-  if (dimSplits2[1] != nTerminals) {
-    stop("Split rows must bear identical labels")
-  }
-  
-  swapSplits <- (dimSplits1[2] > dimSplits2[2])
-  if (swapSplits) {
-    # solve_LDAP expects splits1 to be no larger than splits2
-    tmp <- splits1
-    splits1 <- splits2
-    splits2 <- tmp
+  GeneralizedRF(splits1, splits2, 
+                function(splits1, splits2, nSplits1, nSplits2) {
+    SymmetricDifference <- function (A, B) {
+      (A & !B) | (!A & B)
+    }
     
-    tmp <- dimSplits1
-    dimSplits1 <- dimSplits2
-    dimSplits2 <- tmp
-    
-    remove(tmp)
-  }
-  
-  taxonNames1 <- rownames(splits1)
-  taxonNames2 <- rownames(splits2)
-  
-  if (!is.null(taxonNames2)) {
-    splits2 <- unname(splits2[taxonNames1, , drop=FALSE])
-    splits1 <- unname(splits1) # split2[split1] faster without names
-  }
-  
-  nSplits1 <- dimSplits1[2]
-  nSplits2 <- dimSplits2[2]
-  if (nSplits1 == 0) return (0)
-  
-  SymmetricDifference <- function (A, B) {
-    (A & !B) | (!A & B)
-  }
-  
-  pairScores <- matrix((mapply(function(i, j) {
-    A1 <- splits1[, i]
-    A2 <- splits2[, j]
-    B1 <- !A1
-    B2 <- !A2
-    
-    # Long-winded way:
-    # min(
-    #   sum(SymmetricDifference(A1, A2), SymmetricDifference(B1, B2)),
-    #   sum(SymmetricDifference(A1, B2), SymmetricDifference(B1, A2))
-    # ) / 2L
-    # But SD(A1, A2) == SD(B1, B2) and SD(A1, B2) == SD(B1, A2), so:
-    
-    min(sum(SymmetricDifference(A1, A2)), sum(SymmetricDifference(A1, B2)))
-  },  seq_len(nSplits1), rep(seq_len(nSplits2), each=nSplits1)
-  )), nSplits1, nSplits2)
-  
-  
-  # Return:
-  TreeDistanceReturn(pairScores, maximize = FALSE,
-                     reportMatching, swapSplits,
-                     splits1, splits2,
-                     taxonNames1)
+    matrix((mapply(function(i, j) {
+      A1 <- splits1[, i]
+      A2 <- splits2[, j]
+      B1 <- !A1
+      B2 <- !A2
+      
+      # Long-winded way:
+      # min(
+      #   sum(SymmetricDifference(A1, A2), SymmetricDifference(B1, B2)),
+      #   sum(SymmetricDifference(A1, B2), SymmetricDifference(B1, A2))
+      # ) / 2L
+      # But SD(A1, A2) == SD(B1, B2) and SD(A1, B2) == SD(B1, A2), so:
+      
+      min(sum(SymmetricDifference(A1, A2)), sum(SymmetricDifference(A1, B2)))
+    },  seq_len(nSplits1), rep(seq_len(nSplits2), each=nSplits1)
+    )), nSplits1, nSplits2)
+  }, maximize = FALSE, reportMatching)
 }
