f025f4442f4b71aa9d38f921ee6bc40b04320892
Martin Smith <martins@gmail.com>
2019-11-04 14:56:04 +0000

    copyedit

--- R/zzz.R
+++ R/zzz.R
@@ -7,9 +7,8 @@ release_questions <- function() {
   c(
     "Is the code free of #TODOs?",
     "Have you checked the Vignettes for sanity?",
-    "Have you rebuild the website with pkgdown::build_site()?",
+    "Have you rebuilt the website with pkgdown::build_site()?",
     "Have you refreshed the package meta with codemetar::write_codemeta()?",
-    "Have you pkgdown::build_site()d (if not done by Travis)?",
     "Have you updated REFERENCES.bib with a citation to the published study?",
     "Have you updated inst/CITATION with a citation to the published study?",
     "Have you set 'TreeDist' to a default function?",
