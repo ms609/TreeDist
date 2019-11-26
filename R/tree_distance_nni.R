4550c13ac5c33e0d9e00193c748085efee266877
Martin Smith <martins@gmail.com>
2019-11-26 15:06:43 +0000

    NNI comparisons between multiple trees

--- R/tree_distance_nni.R
+++ R/tree_distance_nni.R
@@ -1,8 +1,37 @@
 #' Approximate the NNI distance
 #' 
-#' An approximation of the NNI distance based on Li et al. 1996.
+#' An approximation of the NNI distance based on Li et al. (1996).
+#' 
+#' In brief, this approximation algorithm works by identifying edges in one
+#' tree that do not match edges in the second.  Each of these edges must
+#' undergo at least one NNI operation in order to reconcile the trees.
+#' Edges that match in both trees need never undergo an NNI operation, and 
+#' divide each tree into smaller regions.  By 'cutting' matched edges into two,
+#' a tree can be divided into a number of regions that solely comprise unmatched
+#' edges.
+#' These regions can be viewed as separate trees that need to be reconciled.
+#' One way to reconcile these trees is to conduct a series of NNI operations
+#' that reduce a tree to a pectinate (caterpillar) tree, then to conduct an 
+#' analogue of the mergesort algorithm.  This takes at most _n_ log _n_ + O(_n_)
+#' NNI operations, and provides a loose upper bound on the NNI score. 
+#' The maximum number of moves for a tree with _n_ tips can be calculated 
+#' exactly for small trees; this provides a tighter upper bound, but is 
+#' unavailable for _n_ > 12.
 #' 
 #' @param tree1,tree2 Individual trees of class `phylo`
+#' 
+#' @return `NNIDist` returns, for each pair of trees, a list containing three
+#' entries.  
+#' 
+#' - `lower` is a lower bound on the NNI distance, and corresponds
+#' to the RF distance between the trees. 
+#' 
+#' - `tight_upper` is an upper bound on the distance, based on calculated
+#' maximum diameters for trees with <12 tips.  _NA_ is returned if trees are
+#' too different to employ this approach.
+#' 
+#' - `loose_upper` is a looser upper bound on the distance, using _n_ log _n_ +
+#' O(_n_).
 #' 
 #' @examples
 #'   library('TreeTools')
@@ -15,18 +44,25 @@
 #'   \insertRef{Li1996}{TreeDist}
 #'   
 #' @template MRS
-#' @importFrom ape Nnode.phylo
-#' @importFrom TreeTools PostorderEdges NTip RenumberTips
 #' @export
-NNIDist <- function (tree1, tree2) {
-  edge <- tree1$edge
-  edge1 <- do.call(cbind, 
-                   PostorderEdges(edge[, 1], edge[, 2], dim(edge)[1], 
-                                  Nnode.phylo(tree1)))
-  tree2 <- RenumberTips(tree2, tree1$tip.label)
-  edge <- tree2$edge
-  edge2 <- do.call(cbind, 
-                   PostorderEdges(edge[, 1], edge[, 2], dim(edge)[1], 
-                                  Nnode.phylo(tree2)))
-  cpp_nni_distance(edge1, edge2, NTip(tree1))
+NNIDist <- function (tree1, tree2 = tree1) {
+  .TreeDistance(.NNIDistSingle, tree1, tree2)
 }
+
+#' @importFrom TreeTools PostorderEdges
+.EdgesInPostorder <- function (edge, nNode) {
+  do.call(cbind, PostorderEdges(edge[, 1], edge[, 2], dim(edge)[1], nNode))
+}
+
+#' @importFrom TreeTools RenumberTips
+#' @importFrom ape Nnode.phylo
+.NNIDistSingle <- function (tree1, tree2, nTip, ...) {
+  tree2 <- RenumberTips(tree2, tree1$tip.label)
+  
+  edge1 <- .EdgesInPostorder(tree1$edge, Nnode.phylo(tree1))
+  edge2 <- .EdgesInPostorder(tree2$edge, Nnode.phylo(tree2))
+  
+  cpp_nni_distance(edge1, edge2, nTip)
+}
+
+
--- tests/testthat/test-tree_distance_nni.R
+++ tests/testthat/test-tree_distance_nni.R
@@ -1,12 +1,12 @@
 context("tree_distance_nni.R")
-library(TreeTools)
-library(ape)
+library('TreeTools')
+library('ape')
 test_that("Simple NNI approximations", {
   nTip <- 6L
   tree1 <- BalancedTree(nTip)
   tree2 <- PectinateTree(nTip)
   edge <- tree1$edge
-  edge1 <- do.call(cbind, 
+  edge1 <- do.call(cbind,
                    PostorderEdges(edge[, 1], edge[, 2], dim(edge)[1], Nnode.phylo(tree1)))
   edge <- tree2$edge
   edge2 <- do.call(cbind,
@@ -16,9 +16,8 @@ test_that("Simple NNI approximations", {
   oneUnmatched <- list(lower = 1L, tight_upper = 1L, loose_upper = 7L)
   fiveUnmatched <- list(lower = 5L, tight_upper = 10L, loose_upper = 17L + 4L)
   expect_equal(oneUnmatched, cpp_nni_distance(edge1, edge2, NTip(tree1)))
-  
   expect_equal(oneUnmatched, NNIDist(tree1, tree2))
-  
+
   # Identical trees
   tree1 <- Postorder(ape::read.tree(text="(((a, b), (c, d)), ((e, f), (g, h)));"))
   tree2 <- Postorder(ape::read.tree(text="(((a, b), (d, c)), ((h, g), (f, e)));"))
@@ -57,3 +56,33 @@ test_that("Simple NNI approximations", {
   expect_true(is.na(NNIDist(ape::rtree(100, br=NULL), ape::rtree(100, br=NULL))[[2]]))
   
 })
+
+test_that("Crashes are avoided", {
+  expect_error(NNIDist(list(PectinateTree(7), PectinateTree(8))))
+  expect_error(NNIDist(list(PectinateTree(1:8), PectinateTree(8))))
+  expect_error(NNIDist(list(PectinateTree(1:8), 
+                            PectinateTree(as.character(1:8)))))
+})
+
+test_that("NNI with lists of trees", {
+  tree1 <- BalancedTree(1:8)
+  list1 <- list(tree1, PectinateTree(as.character(1:8)),
+                PectinateTree(as.character(c(4:1, 5:8))), BalancedTree(c(1:3, 8:4)))
+  
+  multResult <- NNIDist(tree1, list1)
+  expect_equal(NNIDist(tree1, list1[[1]]), multResult[, 1])
+  expect_equal(NNIDist(tree1, list1[[2]]), multResult[, 2])
+  expect_equal(NNIDist(tree1, list1[[3]]), multResult[, 3])
+  expect_equal(NNIDist(tree1, list1[[4]]), multResult[, 4])
+  
+  expect_equal(NNIDist(tree1, list1), NNIDist(list1, tree1))
+  
+  # CompareAll
+  expect_equal(CompareAll(NNIDist, list1), NNIDist(list1))
+  
+  expect_equivalent(
+    lapply(NNIDist(list1), function (x) unname(as.matrix(x)[1:4, 4:1])),
+    NNIDist(list1, rev(list1))
+  )
+})
+  
