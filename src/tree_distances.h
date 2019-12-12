36d05baabd34a1be324e45a5477e1ea1551762a1
Martin Smith <martins@gmail.com>
2019-12-12 15:15:36 +0000

    /64

--- src/tree_distances.h
+++ src/tree_distances.h
@@ -24,7 +24,7 @@ const splitbit ALL_ONES = std::numeric_limits<splitbit>::max();
 const double BIGL = double (BIG);
 /* For a reason I've not estabilshed, dividing by BIG 2 is necessary to avoid 
  * an infinite loop in lap. */
-const cost BIG = (std::numeric_limits<cost>::max() / MAX_SPLITS) / 2;
+const cost BIG = (std::numeric_limits<cost>::max() / MAX_SPLITS) / 64;
 
 const splitbit right16bits = 65535U;
 const uint32_t powers_of_two[16] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512,
