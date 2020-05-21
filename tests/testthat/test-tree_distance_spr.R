1d0d66143159d7ba0e5a82181eff4b1fd97137d4
Martin Smith <martins@gmail.com>
2020-05-21 16:34:30 +0100

    Use phangorn for testing

--- tests/testthat/test-tree_distance_spr.R
+++ tests/testthat/test-tree_distance_spr.R
@@ -2,7 +2,11 @@ context('tree_distance_spr.R')
 
 test_that("SPR.dist called safely", {
   library("TreeTools")
-  expect_equal(c(1,1,1,1,2,2), SPRDist(as.phylo(0:5, 6), BalancedTree(6)))
+  PhangornSPR <- phangorn::SPR.dist
+  expect_equal(PhangornSPR(structure(lapply(as.phylo(0:5, 6), Postorder),
+                                     class = 'multiPhylo'),
+                           Postorder(BalancedTree(6))),
+               SPRDist(as.phylo(0:5, 6), BalancedTree(6)))
   expect_equivalent(SPRDist(BalancedTree(6), PectinateTree(6)),
                     SPRDist(list(BalancedTree(6), PectinateTree(6)))[1])
   
