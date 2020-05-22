8fff2959462e6230dd721db4cad320c19645c4ec
Martin Smith <martins@gmail.com>
2020-05-22 14:43:20 +0100

    t1_descendantsof on heap, not stack
    15% faster and no overflow concerns!

--- src/mast.cpp
+++ src/mast.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <Rcpp.h>
 #include "ints.h"
 using namespace Rcpp;
@@ -20,9 +21,9 @@ int16 max_of_six (const int16 m1,
 }
 
 void fill_M (const int16 node1, const int16 node2,
-             int16 M[][MAST_MAX_ALLNODE],
-             const bool t1_descendantsof[][MAST_MAX_TIP],
-             const bool t2_descendantsof[][MAST_MAX_TIP],
+             int_least16_t M[][MAST_MAX_ALLNODE],
+             const bool* t1_descendantsof,
+             const bool* t2_descendantsof,
              const int16* t1_left,
              const int16* t1_right,
              const int16* t2_left,
@@ -32,11 +33,11 @@ void fill_M (const int16 node1, const int16 node2,
     if (node2 < n_tip) { // Node 1 and node 2 are leaves
       M[node1][node2] = node1 == node2 ? 1 : 0;
     } else { // Node 1 is a leaf; node 2 is internal
-      M[node1][node2] = t2_descendantsof[node2 - n_tip][node1] ? 1 : 0; 
+      M[node1][node2] = t2_descendantsof[(node2 - n_tip) * n_tip + node1] ? 1 : 0; 
     }
   } else {
     if (node2 < n_tip) { // Node 1 is internal; node 2 is a leaf
-      M[node1][node2] = t1_descendantsof[node1 - n_tip][node2] ? 1 : 0;
+      M[node1][node2] = t1_descendantsof[(node1 - n_tip) * n_tip + node2] ? 1 : 0;
     } else { // Node 1 & Node 2 are internal
       const int16 l1 = t1_left[node1 - n_tip],
                   r1 = t1_right[node1 - n_tip],
@@ -52,7 +53,6 @@ void fill_M (const int16 node1, const int16 node2,
     }
   }
 }
-
 
 // edge1 and edge2 should be edge matrices taken from trees of class `phylo`,
 // with one subtracted from all entries so that leaves are numbered from 
@@ -72,8 +72,8 @@ int cpp_mast (IntegerMatrix edge1, IntegerMatrix edge2, IntegerVector nTip) {
     t1_left[MAST_MAX_NODE] = {}, t1_right[MAST_MAX_NODE] = {},
     t2_left[MAST_MAX_NODE] = {}, t2_right[MAST_MAX_NODE] = {};
   bool t1_has_child[MAST_MAX_NODE] = {}, t2_has_child[MAST_MAX_NODE] = {};
-  bool t1_descendantsof[MAST_MAX_NODE][MAST_MAX_TIP] = {},
-       t2_descendantsof[MAST_MAX_NODE][MAST_MAX_TIP] = {};
+  bool *t1_descendantsof = new bool[(n_tip - 1) * n_tip],
+       *t2_descendantsof = new bool[(n_tip - 1) * n_tip];
   for (int16 i = 0; i != n_edge; i++) {
     const int16 
       parent1 = edge1(i, 0), child1 = edge1(i, 1), index1 = parent1 - n_tip,
@@ -92,19 +92,19 @@ int cpp_mast (IntegerMatrix edge1, IntegerMatrix edge2, IntegerVector nTip) {
     }
     
     if (child1 < n_tip) { // Child 1 is a tip
-      t1_descendantsof[index1][child1] = true;
+      t1_descendantsof[index1 * n_tip + child1] = true;
     } else { // Child 1 is a node
       for (int16 tip = 0; tip != n_tip; tip++) {
-        t1_descendantsof[index1][tip] |= 
-          t1_descendantsof[child1 - n_tip][tip];
+        t1_descendantsof[index1 * n_tip + tip] |= 
+          t1_descendantsof[(child1 - n_tip) * n_tip + tip];
       }
     }
     if (child2 < n_tip) {
-      t2_descendantsof[index2][child2] = true;
+      t2_descendantsof[index2 * n_tip + child2] = true;
     } else {
       for (int16 tip = 0; tip != n_tip; tip++) {
-        t2_descendantsof[index2][tip] |= 
-          t2_descendantsof[child2 - n_tip][tip];
+        t2_descendantsof[index2 * n_tip + tip] |= 
+          t2_descendantsof[(child2 - n_tip) * n_tip + tip];
       }
     }
   }
@@ -122,6 +122,9 @@ int cpp_mast (IntegerMatrix edge1, IntegerMatrix edge2, IntegerVector nTip) {
   fill_M(n_tip, n_tip, M, t1_descendantsof,
          t2_descendantsof, t1_left, t1_right,
          t2_left, t2_right, n_tip);
+  
+  delete[] t1_descendantsof;
+  delete[] t2_descendantsof;
   
   return int(M[n_tip][n_tip]);
 }
