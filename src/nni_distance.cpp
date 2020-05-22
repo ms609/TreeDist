eb3e7fb66ff85a7f2d772d809cbf1a4c30814c2b
Martin Smith <martins@gmail.com>
2020-05-22 11:37:51 +0100

    const int16s

--- src/nni_distance.cpp
+++ src/nni_distance.cpp
@@ -14,31 +14,31 @@ const int16 N_EXACT = 12;
 const int16 exact_diameter[] = {0, 0, 0, 0, 1, 3, 5, 7, 10, 12, 15, 18, 21};
 
 int16 sorting_number (int16 n_tip) {
-  int log_ceiling = std::ceil(log2(n_tip));
+  const int16 log_ceiling = std::ceil(log2(n_tip));
   return (n_tip * log_ceiling - std::pow(2, log_ceiling) + 1);
 }
 
-int16 degenerate_distance (int16 n_tip) {
+int16 degenerate_distance (const int16 n_tip) {
   /* Calculate the shortest length of the longest path in a tree.
    * To make this path as short as possible, divide tips into three 
    * balanced trees, joined by a single node that will form part of every 
    * longest path.  One of these subtrees will be filled with >= n/3 nodes */
-  int16 nodes_in_full = (std::ceil(log2(n_tip / 3)) > 0) ? 
+  const int16 nodes_in_full = (std::ceil(log2(n_tip / 3)) > 0) ? 
   std::ceil(log2(n_tip / 3)) : 0;
   /* We want to put a power of two tips in this subtree, such that every node is 
    * equally close to its root */
-  int16 tips_in_full = std::pow(2, nodes_in_full);
+  const int16 tips_in_full = std::pow(2, nodes_in_full);
   /* Now the remaining tips must be spread sub-evenly between the remaining 
    * edges from this node.  Picture halving the tips; removing tips from one side
    * until it is a power of two will reduce the number of nodes by one, whilst 
    * at worst (if this brings the other side over a power of two) increasing the 
    * other side by one. */
-  int16 tips_left = n_tip - tips_in_full;
-  int16 min_backbone_nodes = 
+  const int16 tips_left = n_tip - tips_in_full;
+  const int16 min_backbone_nodes = 
     (nodes_in_full + std::ceil(log2(tips_left / 2)) + 1 > 0) ?
      nodes_in_full + std::ceil(log2(tips_left / 2)) + 1 : 0;
   /* The worst-case scenario requires a move for every node not on the backbone: */
-  int16 n_node = (n_tip > 2) ? n_tip - 2 : 0;
+  const int16 n_node = (n_tip > 2) ? n_tip - 2 : 0;
   return (n_node - min_backbone_nodes);
 }
 
@@ -65,10 +65,10 @@ IntegerVector cpp_nni_distance (IntegerMatrix edge1, IntegerMatrix edge2,
     throw std::length_error("Cannot calculate NNI distance for trees with "
                               "so many tips.");
   }
-  int16 n_tip = nTip[0],
-               node_0 = n_tip,
-               node_0_r = n_tip + 1,
-               n_edge = edge1.nrow();
+  const int16 n_tip = nTip[0],
+              node_0 = n_tip,
+              node_0_r = n_tip + 1,
+              n_edge = edge1.nrow();
   int lower_bound = 0, tight_score_bound = 0, loose_score_bound = 0;
   if (n_tip < 4) {
     return(IntegerVector::create(Named("lower") = 0,
