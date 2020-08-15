5f211cab940bfa22d9a4d08629484579c6605882
Martin Smith <martins@gmail.com>
2020-08-15 21:16:21 +0100

    copy lap→soft_lap()

--- src/lap.cpp
+++ src/lap.cpp
@@ -341,3 +341,255 @@ cost lap(int16 dim,
   
   return lapcost;
 }
+
+/* Copy of lap, but allowing for rounding errors */
+cost soft_lap(int16 dim,
+              cost **input_cost,
+              lap_col *rowsol,
+              lap_row *colsol,
+              cost *u,
+              cost *v) {
+  bool unassignedfound;
+  lap_row  i, imin, num_free = 0, previous_num_free, f, i0, k, free_row,
+           *predecessor, *free;
+  lap_col  j, j1, j2 = 0, endofpath = 0, last = 0, low, up, *col_list, *matches;
+  /* Initializing min, endofpath, j2 and last is unnecessary, 
+   * but avoids compiler warnings */
+  cost min = 0, h, umin, usubmin, v2, *d;
+  
+  free = new lap_row[dim];        // List of unassigned rows.
+  col_list = new lap_col[dim];    // List of columns to be scanned in various ways.
+  matches = new lap_col[dim];     // Counts how many times a row could be assigned.
+  d = new cost[dim];              // 'Cost-distance' in augmenting path calculation.
+  predecessor = new lap_row[dim]; // Row-predecessor of column in augmenting/alternating path.
+  
+  // Init how many times a row will be assigned in the column reduction.
+  for (i = 0; i != dim; i++) {
+    matches[i] = 0;
+  }
+  
+  // COLUMN REDUCTION
+  for (j = dim; j--; ) { // Reverse order gives better results.
+    // Find minimum cost over rows.
+    min = input_cost[0][j];
+    imin = 0;
+    for (i = 1; i != dim; i++) {
+      if (input_cost[i][j] < min) {
+        min = input_cost[i][j];
+        imin = i;
+      }
+    }
+    v[j] = min;
+    if (++matches[imin] == 1) {
+      // Init assignment if minimum row assigned for first time.
+      rowsol[imin] = j;
+      colsol[j] = imin;
+    } else if (v[j] < v[rowsol[imin]]) {
+      int16 j1 = rowsol[imin];
+      rowsol[imin] = j;
+      colsol[j] = imin;
+      colsol[j1] = -1;
+    } else {
+      colsol[j] = -1; // Row already assigned, column not assigned.
+    }
+  }
+  
+  // REDUCTION TRANSFER
+  for (i = 0; i != dim; i++) {
+    if (matches[i] == 0) { // Fill list of unassigned 'free' rows.
+      free[num_free++] = i;
+    } else {
+      if (matches[i] == 1) { // Transfer reduction from rows that are assigned once.
+        j1 = rowsol[i];
+        min = BIG;
+        for (j = 0; j < dim; j++) {
+          if (j != j1) {
+            if (input_cost[i][j] - v[j] < min) {
+              min = input_cost[i][j] - v[j];
+            }
+          }
+        }
+        v[j1] = v[j1] - min;
+      }
+    }
+  }
+  
+  //   AUGMENTING ROW REDUCTION
+  int16 loopcnt = 0;           // do-loop to be done twice.
+  do {
+    loopcnt++;
+    
+    //     Scan all free rows.
+    //     In some cases, a free row may be replaced with another one to be 
+    //     scanned next.
+    k = 0;
+    previous_num_free = num_free;
+    num_free = 0;             // Start list of rows still free after augmenting
+                              // row reduction.
+    while (k < previous_num_free) {
+      i = free[k];
+      k++;
+      
+      //     Find minimum and second minimum reduced cost over columns.
+      umin = input_cost[i][0] - v[0];
+      j1 = 0;
+      usubmin = BIG;
+      for (j = 1; j < dim; j++) {
+        h = input_cost[i][j] - v[j];
+        if (h < usubmin) {
+          if (h >= umin) {
+            usubmin = h;
+            j2 = j;
+          } else {
+            usubmin = umin;
+            umin = h;
+            j2 = j1;
+            j1 = j;
+          }
+        }
+      }
+      
+      i0 = colsol[j1];
+      if (nontrivially_less_than(umin, usubmin)) {
+        //  Change the reduction of the minimum column to increase the minimum
+        //  reduced cost in the row to the subminimum.
+        v[j1] = v[j1] - (usubmin - umin);
+      } else if (i0 > -1) {
+          // Minimum and subminimum equal.
+          // Minimum column j1 is assigned.
+          // Swap columns j1 and j2, as j2 may be unassigned.
+          j1 = j2;
+          i0 = colsol[j2];
+      }
+        
+      //    (Re-)assign i to j1, possibly de-assigning an i0.
+      rowsol[i] = j1;
+      colsol[j1] = i;
+      
+      if (i0 > -1) { // Minimum column j1 assigned earlier.
+        if (nontrivially_less_than(umin, usubmin)) {
+          // Put in current k, and go back to that k.
+          // Continue augmenting path i - j1 with i0.
+          free[--k] = i0;
+          Rcpp::checkUserInterrupt();
+        } else {
+          // No further augmenting reduction possible.
+          // Store i0 in list of free rows for next phase.
+          free[num_free++] = i0;
+        }
+      }
+    }
+  } while (loopcnt < 2); // Repeat once.
+  
+  // AUGMENT SOLUTION for each free row.
+  for (f = 0; f != num_free; f++) {
+    free_row = free[f];       // Start row of augmenting path.
+    
+    // Dijkstra shortest path algorithm.
+    // Runs until unassigned column added to shortest path tree.
+    for(j = dim; j--;) {
+      d[j] = input_cost[free_row][j] - v[j];
+      predecessor[j] = free_row;
+      col_list[j] = j;        // Init column list.
+    }
+    
+    low = 0; // Columns in 0..low-1 are ready, now none.
+    up = 0;  // Columns in low..up-1 are to be scanned for current minimum, now none.
+    // Columns in up..dim-1 are to be considered later to find new minimum;
+    // at this stage the list simply contains all columns.
+    unassignedfound = false;
+    do {
+      if (up == low) { // No more columns to be scanned for current minimum.
+        last = low - 1;
+        
+        // Scan columns for up..dim-1 to find all indices for which new minimum occurs.
+        // Store these indices between low..up-1 (increasing up).
+        min = d[col_list[up++]];
+        for (k = up; k < dim; k++) {
+          j = col_list[k];
+          h = d[j];
+          if (h <= min) {
+            if (h < min) {   // New minimum.
+              up = low;      // Restart list at index low.
+              min = h;
+            }
+            // New index with same minimum, put on undex up, and extend list.
+            col_list[k] = col_list[up];
+            col_list[up++] = j;
+          }
+        }
+        // Check if any of the minimum columns happens to be unassigned.
+        // If so, we have an augmenting path right away.
+        for (k = low; k != up; k++) {
+          if (colsol[col_list[k]] < 0) {
+            endofpath = col_list[k];
+            unassignedfound = true;
+            break;
+          }
+        }
+      }
+      
+      if (!unassignedfound) {
+        // Update 'distances' between free_row and all unscanned columns,
+        // via next scanned column.
+        j1 = col_list[low];
+        low++;
+        i = colsol[j1];
+        h = input_cost[i][j1] - v[j1] - min;
+        
+        for (k = up; k != dim; k++) {
+          j = col_list[k];
+          v2 = input_cost[i][j] - v[j] - h;
+          if (v2 < d[j]) {
+            predecessor[j] = i;
+            if (v2 == min) { // New column found at same minimum value
+              if (colsol[j] < 0) {
+                // If unassigned, shortest augmenting path is complete.
+                endofpath = j;
+                unassignedfound = true;
+                break;
+              } else {
+              // Else add to list to be scanned right away.
+                col_list[k] = col_list[up];
+                col_list[up++] = j;
+              }
+            }
+            d[j] = v2; // <MS: Unintended>
+          }
+        }
+      }
+    } while (!unassignedfound);
+    
+    // Update column prices.
+    for(k = last + 1; k--;) {
+      j1 = col_list[k];
+      v[j1] = v[j1] + d[j1] - min;
+    }
+    
+    // Reset row and column assignments along the alternating path.
+    do {
+      i = predecessor[endofpath];
+      colsol[endofpath] = i;
+      j1 = endofpath;
+      endofpath = rowsol[i];
+      rowsol[i] = j1;
+    } while (i != free_row);
+  }
+  
+  // Calculate optimal cost.
+  cost lapcost = 0;
+  for(i = dim; i--;) {
+    j = rowsol[i];
+    u[i] = input_cost[i][j] - v[j];
+    lapcost = lapcost + input_cost[i][j];
+  }
+  
+  // Free reserved memory.
+  delete[] predecessor;
+  delete[] free;
+  delete[] col_list;
+  delete[] matches;
+  delete[] d;
+  
+  return lapcost;
+}
