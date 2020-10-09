4ecab33f3fbdb88ad49046970f782288e661b1f0
Martin Smith <martins@gmail.com>
2020-10-09 09:20:11 +0100

    Update test-day_1985.cpp.r

--- tests/testthat/test-day_1985.cpp.r
+++ tests/testthat/test-day_1985.cpp.r
@@ -35,15 +35,14 @@ test_that("Day 1985 examples", {
                  NSplits(c(t2, t3))),
                as.integer(RobinsonFoulds(c(t1, t2, t3, tStar))))
   
+  # Day's figure 2
   t1 <- PrepareTree("((10, 7), (6, (8, 11)), (12, (4, (2, 1))), 14, (5, 9, 13), 3);")
-  t2 <- PrepareTree("(((2, 4, 5, 7, 9, 10, 12, 13), (1, 14)), (6, (8, 11)), 3);")
+  as.ClusterTable(t1)
+  t2 <- PrepareTree("(((2, 4, 5, 7, 9, 10, 12, 13), 1, 14), (6, (8, 11)), 3);")
+  as.ClusterTable(t2)
   
-  COMCLUST(list(t1, t2))
-  expect_null(NULL)
-  t1n <- structure(list(edge = structure(
-    c(22, 15, 15, 22, 19, 19, 16, 16, 22, 21, 21, 20, 20, 17, 17, 22, 22, 18, 18, 
-      18, 22, 15, 10L, 7L, 19, 6L, 16, 8L, 11L, 21, 12L, 20, 
-      4L, 17, 2L, 1L, 14L, 18, 5L, 9L, 13L, 3L), .Dim = c(21L, 2L)),
-    Nnode = 8L, tip.label = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14")), class = "phylo")
-  
+  expect_equal(2L, COMCLUST(list(t1, t2)))
+  expect_equal(7L, as.integer(RobinsonFoulds(list(t1, t2))))
+
 })
+  
