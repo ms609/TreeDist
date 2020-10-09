376dfd3acdf5703539b706788de5886eb7e414f1
Martin Smith <martins@gmail.com>
2020-10-09 08:44:19 +0100

    Rename test

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -64,7 +64,7 @@ test_that('VisualizeMatching() works', {
                       leaveRoom = FALSE)
   })
   
-  expect_doppelganger('Collapse a node', function () {
+  expect_doppelganger('RF: Collapse a node', function () {
     par(mfrow = c(2, 2), mar = rep(0.1, 4), cex = 1.5)
     tree1 <- ape::read.tree(text='((1, 2), ((6, (7, 8)), (3, 4, (5, 9))));')
     tree2 <- ape::read.tree(text='((1, 2), ((3, (4, (5, 9))), (6, (7, 8))));')
