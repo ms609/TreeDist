c259a9777e948b3cf0f933ba59c8bf1502f7bfe1
Martin Smith <martins@gmail.com>
2021-04-30 10:24:55 +0100

    pass `nn`, and micro-improvements

--- R/spectral_clustering.R
+++ R/spectral_clustering.R
@@ -13,23 +13,23 @@
 #' SpectralClustering(distances)
 #' @family tree space functions
 #' @export
-SpectralClustering <- function(D, nn = 10, nEig = 2) {
-  MutualKnnGraph <- function(D, nn = 10) {
+SpectralClustering <- function (D, nn = 10L, nEig = 2L) {
+  
+  MutualKnnGraph <- function (D, nn) {
     D <- as.matrix(D)
+    dims <- dim(D)
     
     # intialize the knn matrix
-    knn_mat <- matrix(0, nrow = nrow(D), ncol = nrow(D))
+    knn_mat <- matrix(FALSE, nrow = dims[1], ncol = dims[2])
     
     # find the 10 nearest neighbors for each point
     for (i in seq_len(nrow(D))) {
       neighbor_index <- order(D[i, ])[2:(nn + 1)]
-      knn_mat[i, ][neighbor_index] <- 1
+      knn_mat[i, ][neighbor_index] <- TRUE
     }
     
     # Now we note that i,j are neighbors iff K[i,j] = 1 or K[j,i] = 1
-    knn_mat <- knn_mat + t(knn_mat) # find mutual knn
-    
-    knn_mat[knn_mat == 2] <- 1
+    knn_mat <- knn_mat | t(knn_mat) # find mutual knn
     
     # Return:
     knn_mat
@@ -38,15 +38,15 @@ SpectralClustering <- function(D, nn = 10, nEig = 2) {
   GraphLaplacian <- function(W) {
     stopifnot(nrow(W) == ncol(W))
     
-    g = colSums(W) # degrees of vertices
-    n = nrow(W)
+    g <- colSums(W) # degrees of vertices
+    n <- dim(W)[1]
+    D_half <- diag(1 / sqrt(g))
     
     # Return:
-    D_half = diag(1 / sqrt(g))
     diag(n) - D_half %*% W %*% D_half
   }
   
-  W <- MutualKnnGraph(D) # 1. matrix of similarities
+  W <- MutualKnnGraph(D, nn) # 1. matrix of similarities
   L <- GraphLaplacian(W) # 2. compute graph laplacian
   ei <- eigen(L, symmetric = TRUE) # 3. Compute the eigenvectors and values of L
   
