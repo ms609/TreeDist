1852090db692895a750ed907c6df5d4ff2f400a5
Martin Smith <martins@gmail.com>
2021-06-30 12:25:41 +0100

    const

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -50,13 +50,13 @@ BEGIN_RCPP
 END_RCPP
 }
 // consensus_info
-double consensus_info(List trees, LogicalVector phylo);
+double consensus_info(const List trees, const LogicalVector phylo);
 RcppExport SEXP _TreeDist_consensus_info(SEXP treesSEXP, SEXP phyloSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< List >::type trees(treesSEXP);
-    Rcpp::traits::input_parameter< LogicalVector >::type phylo(phyloSEXP);
+    Rcpp::traits::input_parameter< const List >::type trees(treesSEXP);
+    Rcpp::traits::input_parameter< const LogicalVector >::type phylo(phyloSEXP);
     rcpp_result_gen = Rcpp::wrap(consensus_info(trees, phylo));
     return rcpp_result_gen;
 END_RCPP
