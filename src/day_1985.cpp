127ee0be510c9d04d20e0f30ef85fa854f4d8da7
Martin Smith <martins@gmail.com>
2021-07-01 17:22:24 +0100

    precalculate Log2[un]rooted

--- src/day_1985.cpp
+++ src/day_1985.cpp
@@ -12,32 +12,35 @@ const int16
   UNINIT = -999
 ;
 
-const int
-  DAY_MAX_LEAVES = 16384,
+const int_fast32_t
+  DAY_MAX_LEAVES = 16383,
   FACT_MAX = DAY_MAX_LEAVES + DAY_MAX_LEAVES + 5 + 1
 ;
 
 const double log_2 = log(2);
 double ldfact[FACT_MAX];
+double l2rooted[DAY_MAX_LEAVES];
+double l2unrooted[DAY_MAX_LEAVES];
 __attribute__((constructor))
   void compute_double_factorials() {
-    int_fast32_t i;
     ldfact[0] = 0;
     ldfact[1] = 0;
-    ldfact[2] = 1;
+    l2rooted[0] = 0;
+    l2rooted[1] = 0;
+    l2rooted[2] = 0;
+    l2unrooted[0] = 0;
+    l2unrooted[1] = 0;
+    l2unrooted[2] = 0;
     
-    for (i = 3; i != FACT_MAX; i++)
+    for (int_fast32_t i = 2; i != FACT_MAX; i++) {
       ldfact[i] = ldfact[i - 2] + log2(i);
-    
+    }
+
+    for (int_fast32_t i = 3; i != DAY_MAX_LEAVES; i++) {
+      l2rooted[i] = ldfact[i + i - 3];
+      l2unrooted[i] = ldfact[i + i - 5];
+    }
   }
-
-inline double l2rooted(const int_fast32_t n_tips) {
-  return n_tips < 2L ? 0. : ldfact[n_tips + n_tips - 3];
-}
-
-inline double l2unrooted(const int16 *n_tips) {
-  return *n_tips < 3 ? 0. : ldfact[int_fast32_t(*n_tips) + *n_tips - 5];
-}
 
 inline double split_phylo_info (const int16 n_in, const int16 *n_tip,
                                 const double p) {
@@ -45,14 +48,12 @@ inline double split_phylo_info (const int16 n_in, const int16 *n_tip,
   assert(p > 0);
   assert(p <= 1);
   if (p == 1) {
-    return (l2unrooted(n_tip) -
-            l2rooted(n_in)-
-            l2rooted(n_out));
+    return (l2unrooted[*n_tip] - l2rooted[n_in] - l2rooted[n_out]);
   } else {
     const double 
       q = 1 - p,
-      l2n = l2unrooted(n_tip),
-      l2n_consistent = l2rooted(n_in) + l2rooted(n_out),
+      l2n = l2unrooted[*n_tip],
+      l2n_consistent = l2rooted[n_in] + l2rooted[n_out],
       l2p_consistent = l2n_consistent - l2n,
       l2p_inconsistent = log2(-expm1(l2p_consistent * log_2)),
       l2n_inconsistent = l2p_inconsistent + l2n
