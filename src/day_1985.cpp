3704b79f24f3f8c6efeef15fe2f8bdcf5c0d44a5
Martin Smith <martins@gmail.com>
2021-07-04 18:49:59 +0100

    increment first

--- src/day_1985.cpp
+++ src/day_1985.cpp
@@ -562,14 +562,14 @@ double consensus_info (const List trees, const LogicalVector phylo) {
               if (tables[i].CLUSTONL(&L, &R)) {
                 tables[j].SETSWX(&j_pos);
                 assert(L > 0);
-                split_count[L - 1]++;
+                ++split_count[L - 1];
                 if (!split_size[L - 1]) {
                   split_size[L - 1] = R - L + 1;
                 }
               } else if (tables[i].CLUSTONR(&L, &R)) {
                 tables[j].SETSWX(&j_pos);
                 assert(R > 0);
-                split_count[R - 1]++;
+                ++split_count[R - 1];
                 if (!split_size[R - 1]) {
                   split_size[R - 1] = R - L + 1;
                 }
