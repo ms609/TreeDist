daeb1d75d56c6e6a27faa3366cdc1df2095572b5
Martin Smith <martins@gmail.com>
2021-07-19 10:22:29 +0100

    alpha

--- src/tree_distances.cpp
+++ src/tree_distances.cpp
@@ -1,5 +1,5 @@
-#include <memory> /* for unique_ptr, make_unique */
 #include <cmath>
+#include <memory> /* for unique_ptr, make_unique */
 #include <Rcpp.h>
 #include "tree_distances.h"
 #include "SplitList.h"
