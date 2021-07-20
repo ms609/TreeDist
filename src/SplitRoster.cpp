0eff372c6bda5b834305169037ce512eceb3f9f1
Martin Smith <martins@gmail.com>
2021-07-20 12:02:00 +0100

    mvoe up

--- src/SplitRoster.cpp
+++ src/SplitRoster.cpp
@@ -5,6 +5,72 @@
 #include "SplitRoster.hpp"
 using namespace Rcpp;
 
+SplitRoster::SplitRoster(const List x, const IntegerVector nTip) {
+  n_tips = nTip[0];
+  n_trees = x.length();
+  splits.reserve(n_trees);
+  
+  for (int32 i = 0; i != n_trees; ++i) {
+    const RawMatrix xi = x[i];
+    splits.emplace_back(xi);
+    splits[i].quicksort();
+  }
+  n_bins = splits[0].n_bins;
+  
+  const int16 max_splits = n_tips - 3;
+  roster_tree = std::make_unique<int32[]>(n_trees * max_splits);
+  roster_split = std::make_unique<int16[]>(n_trees * max_splits);
+  roster_size = std::make_unique<int16[]>(n_trees * max_splits);
+  roster_hits = std::make_unique<int32[]>(n_trees * max_splits);
+  index.reserve(n_trees);
+  
+  // Populate roster using k-way merge with tournament tree
+  const int32
+    tournament_games = n_trees - 1,
+      tournament_nodes = n_trees + tournament_games
+    ;
+  
+  auto winners = std::make_unique<int32[]>(tournament_nodes);
+  auto losers = std::make_unique<int32[]>(tournament_nodes);
+  auto which_split = std::make_unique<int16[]>(n_trees);
+  
+  
+  // Populate children of tree
+  for (int32 i = 0; i != n_trees; ++i) {
+    which_split[i] = splits[i].n_splits - 1;
+    winners[i + tournament_games] = i;
+  }
+  for (int32 i = n_trees + tournament_games; i != tournament_nodes; ++i) {
+    which_split[i] = -1;
+    winners[i] = -1;
+  }
+  
+  // Initial games
+  for (int32 i = tournament_games; i--; ) {
+    play_game(&i, winners, losers, which_split);
+  }
+  
+  roster_pos = 0;
+  roster_tree[0] = winners[0];
+  roster_split[0] = which_split[winners[0]];
+  roster_hits[0] = 1;
+  
+  for (; ; ) {
+    const int32 winner = winners[0];
+    --which_split[winner];
+    
+    int32 i = winner;
+    do {
+      i /= 2;
+      play_game(&i, winners, losers, which_split);
+    } while (i);
+    
+    if (which_split[winners[0]] < 0) break;
+    
+    push(winners[0], which_split);
+  }
+}
+
 inline bool SplitRoster::splits_equal(
     const splitbit (&a)[MAX_SPLITS][MAX_BINS], const int16 split_a,
     const splitbit (&b)[MAX_SPLITS][MAX_BINS], const int16 split_b) {
@@ -78,35 +144,6 @@ inline void SplitRoster::push(
   index[tree][new_split] = roster_pos;
 }
 
-SplitRoster::SplitRoster(const List x, const IntegerVector nTip) {
-  n_tips = nTip[0];
-  n_trees = x.length();
-  splits.reserve(n_trees);
-  
-  for (int32 i = 0; i != n_trees; ++i) {
-    const RawMatrix xi = x[i];
-    splits.emplace_back(xi);
-    splits[i].quicksort();
-  }
-  n_bins = splits[0].n_bins;
-  
-  const int16 max_splits = n_tips - 3;
-  roster_tree = std::make_unique<int32[]>(n_trees * max_splits);
-  roster_split = std::make_unique<int16[]>(n_trees * max_splits);
-  roster_size = std::make_unique<int16[]>(n_trees * max_splits);
-  roster_hits = std::make_unique<int32[]>(n_trees * max_splits);
-  index.reserve(n_trees);
-  
-  // Populate roster using k-way merge with tournament tree
-  const int32
-    tournament_games = n_trees - 1,
-    tournament_nodes = n_trees + tournament_games
-  ;
-  
-  auto winners = std::make_unique<int32[]>(tournament_nodes);
-  auto losers = std::make_unique<int32[]>(tournament_nodes);
-  auto which_split = std::make_unique<int16[]>(n_trees);
-  
   
   // Populate children of tree
   for (int32 i = 0; i != n_trees; ++i) {
