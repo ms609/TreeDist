9f4e42922e3da7fcb56a425921a4dfd68825b05a
Martin Smith <martins@gmail.com>
2021-07-21 19:40:35 +0100

    Working for 9 leaves

--- src/SplitList.cpp
+++ src/SplitList.cpp
@@ -72,33 +72,32 @@ SplitList::SplitList(RawMatrix x, int16 n_tips) {
                             "Please contact the TreeDist maintainer if you "    // # nocov
                             "need to use more!");                               // # nocov
   }
-  // Rcout << "\n=== SplitList::SplitList(x, n_tip) ===\n";
-  const int16 last_bin_tips = INLASTBIN(n_tip, R_BIN_SIZE);
-  // Rcout << "Last bt: " << last_bin_tips << ".\n";
-  // Rcout << "Last 2^bt: " << (2^last_bin_tips) << ".\n";
-  const splitbit last_mask = std::pow(2, last_bin_tips) - 1;
-  // Rcout << "Last mask: " << last_mask << ".\n";
-  Rcout << "\n=== SplitList::SplitList(x, n_tips) ===\n";
+  // Rcout << "\n=== SplitList::SplitList(x, n_tips) ===\n";
   
   for (int16 split = 0; split != n_splits; split++) {
     int16 last_bin = n_bins - 1;
     const int16 raggedy_bins = INLASTBIN(n_input_bins, R_BIN_SIZE);
     
-    splitbit last = INSUBBIN(last_bin, 0);
-    bool invert = last & splitbit(1);
-    // Rcout << n_input_bins << " bins in; " << raggedy_bins << " raggedy bins\n";
-    // Rcout << "Last bin: " << last << "; intersection with " << splitbit(1)
-    //       << " = " << invert << "; " << (invert ? "inverting" : "Retaining")
-    //       << ".\n";
-    state[split][last_bin] = invert ? ~last & last_mask : last;
+    Rcout << n_input_bins << " bins in; " << raggedy_bins << " raggedy bins\n";
+    assert(raggedy_bins <= BIN_SIZE / R_BIN_SIZE);
+    assert(raggedy_bins > 0);
+    state[split][last_bin] = INSUBBIN(last_bin, 0);
     // Rcout << " State[" << split << "][" << last_bin << "] = " << state[split][last_bin] << ".\n";
     for (int16 input_bin = 1; input_bin != raggedy_bins; input_bin++) {
-      // Rcout << "Adding " << (splitbit (x(split, (last_bin * 2) + input_bin))) << " << "
-      //       << (R_BIN_SIZE * input_bin) << " to state [" << split << "][" << last_bin
-      //       << "], was " << state[split][last_bin] << "\n";
-      state[split][last_bin] += invert ? 
-        ~INBIN(input_bin, last_bin):
-         INBIN(input_bin, last_bin);
+      // Rcout << " state [" << split << "][" << last_bin
+      //       << "] increased from " << state[split][last_bin];
+      state[split][last_bin] += INBIN(input_bin, last_bin);
+      // Rcout << " by " << INBIN(input_bin, last_bin) << "\n";
+    }
+    const bool invert = state[split][last_bin] & splitbit(1);
+    if (invert) {
+      const int16 last_bin_tips = INLASTBIN(n_tips, BIN_SIZE);
+      // Rcout << "Last bin tips: " << last_bin_tips << ".\n";
+      
+      const splitbit last_mask = std::pow(2, INLASTBIN(n_tips, BIN_SIZE)) - 1;
+      // Rcout << "Last mask: " << last_mask << ".\n";
+      
+      state[split][last_bin] = ~state[split][last_bin] & last_mask;
     }
     in_split[split] = count_bits(state[split][last_bin]);
     
@@ -107,11 +106,12 @@ SplitList::SplitList(RawMatrix x, int16 n_tips) {
       state[split][bin] = invert ? ~INSUBBIN(bin, 0) : INSUBBIN(bin, 0);
       for (int16 input_bin = 1; input_bin != input_bins_per_bin; input_bin++) {
         // Rcout << "Adding " << (splitbit (x(split, (bin * 2) + input_bin))) << " << "
-        //       << (R_BIN_SIZE * input_bin) << " to state [" << split << "][" 
+        //       << (R_BIN_SIZE * input_bin) << " to state [" << split << "]["
         //       << bin << "], was " << state[split][bin] << "\n";
-        state[split][bin] += invert ?
-        ~INSUBBIN(bin, input_bin):
-         INSUBBIN(bin, input_bin);
+        state[split][bin] += INSUBBIN(bin, input_bin);
+      }
+      if (invert) {
+        state[split][bin] = ~state[split][bin];
       }
       in_split[split] += count_bits(state[split][bin]);
     }
