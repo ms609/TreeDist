c4eb4b12e5a95298a1a4a501abae21a8856fb238
Martin Smith <martins@gmail.com>
2021-08-03 12:51:25 +0100

    Rcpp v1.0.7

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // ClusterTable_new
 SEXP ClusterTable_new(List phylo);
 RcppExport SEXP _TreeDist_ClusterTable_new(SEXP phyloSEXP) {
