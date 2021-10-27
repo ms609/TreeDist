c989d3e2effe5fceafeb0a47a7c3159e0d98ad49
Martin Smith <1695515+ms609@users.noreply.github.com>
2021-10-27 12:00:49 +0100

    missing line

--- tests/testthat/test-split_info.R
+++ tests/testthat/test-split_info.R
@@ -12,6 +12,7 @@ test_that("Split info calculated", {
   trees[] <- lapply(trees, RenumberTips, c(letters[1:7], 'x', 'z'))
   expect_equal(consensus_info(trees, TRUE, 1), 0)
   expect_equal(SplitwiseInfo(consensus(trees, p = 0.5), p = split_p),
+               ConsensusInfo(trees, p = 0.5))
   expect_warning(expect_equal(ConsensusInfo(trees, p = 0.4),
                               ConsensusInfo(trees, p = 0.5)))
   expect_equal(SplitwiseInfo(consensus(trees, p = 0.7),
