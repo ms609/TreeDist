13887b333e38444751ea388c7d74e7eeaa1e2d7b
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-01-03 20:17:23 +0000

    Manual checks now automated

--- R/zzz.R
+++ R/zzz.R
@@ -13,24 +13,16 @@ release_questions <- function() {
 
 # Additional steps:
 #
-# Propogate changes in README.md to R/TreeDist-package.R
+# codemeta::write_codemeta()
+# 
+# Propagate changes in README.md to R/TreeDist-package.R
 
 # Additional tests:
-# 
-# spell_check()
-# pkgdown::build_reference_index()
 #
 # run_examples()
 # build_vignettes()
 # build_manual() # PDF support for special characters
-#
-# devtools::check_win_devel(quiet = TRUE); rhub::check_for_cran()
-# Check valgrind section of GitHub actions for memcheck errors
-# 
-# revdepcheck::revdep_check()
-#
-# codemetar::write_codemeta()
-#
+
 # # Unnecessary:
 # # tools::resaveRdaFiles('R', compress='auto') - is default bzip2 the optimal?
 # # tools::checkRdaFiles('R') - set optimal compression in `data-raw`
