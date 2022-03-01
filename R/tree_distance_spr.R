364c5216998b66befc62b76e7fddd9e4dcd9702f
Martin R. Smith <pjjg18@durham.ac.uk>
2022-03-01 19:45:27 +0000

    Split out deO approach

--- R/tree_distance_spr.R
+++ R/tree_distance_spr.R
@@ -124,7 +124,7 @@ SPRDist.multiPhylo <- SPRDist.list
     i <- rep(seq_len(nSplits), nSplits)
     j <- rep(seq_len(nSplits), each = nSplits)
     mmSize <- mismatch_size(sp[[1]], sp[[2]])
-    # which(TipsInSplits(xor.Splits(sp[[1]][[i]], sp[[2]][[j]])) - mmSize > 0)
+    
     minMismatch <- which.min(mmSize)
     if (mmSize[minMismatch] == 0) {
       agreement <- as.logical(sp[[1]][[i[minMismatch]]])
@@ -140,119 +140,102 @@ SPRDist.multiPhylo <- SPRDist.list
              )
     }
     
-    if (TRUE) { # Experimental way
-        
-      splitA <- sp[[1]][[i[minMismatch]]]
-      splitB <- sp[[2]][[j[minMismatch]]]
-      ins <- TipsInSplits(c(splitA, splitB, splitA & splitB),
-                          keep.names = FALSE)
-      nTip <- attr(splitA, "nTip")
-      AB <- ins[3]
-      aB <- ins[2] - ins[3]
-      Ab <- ins[1] - ins[3]
-      ab <- nTip - (aB + Ab + AB)
-      confusion <- c(AB = AB, ab = ab, aB = aB, Ab = Ab)
-      if (debug) {
-        summary(c(splitA, splitB))
-        print(confusion)
-      }
-      balance <- ins[1:2] + ins[1:2] - nTip
-      absBal <- abs(balance)
-      # balance > 0 means more in than out
-      confusInf <- ifelse(confusion == 0, Inf, confusion)
-      mins <- confusion == min(confusInf)
-      drop <- switch(sum(mins),
-        switch(which(mins),
-               as.logical(splitA & splitB),
-               as.logical(!splitA & !splitB),
-               as.logical(!splitA & splitB),
-               as.logical(splitA & !splitB)),
-        {
-          if (absBal[1] > absBal[2]) {
-            # remove from A, which is less balanced
-            if (balance[1] < 0) {
-              # remove from IN A
-              if (confusion[1] == confusion[4]) {
-                as.logical(splitA)
-              } else if (confusInf[1] < confusInf[4]) {
-                as.logical(splitA & splitB)
-              } else {
-                as.logical(splitA & !splitB)
-              }
+    splitA <- sp[[1]][[i[minMismatch]]]
+    splitB <- sp[[2]][[j[minMismatch]]]
+    ins <- TipsInSplits(c(splitA, splitB, splitA & splitB),
+                        keep.names = FALSE)
+    nTip <- attr(splitA, "nTip")
+    AB <- ins[3]
+    aB <- ins[2] - ins[3]
+    Ab <- ins[1] - ins[3]
+    ab <- nTip - (aB + Ab + AB)
+    confusion <- c(AB = AB, ab = ab, aB = aB, Ab = Ab)
+    if (debug) {
+      summary(c(splitA, splitB))
+      print(confusion)
+    }
+    balance <- ins[1:2] + ins[1:2] - nTip
+    absBal <- abs(balance)
+    # balance > 0 means more in than out
+    confusInf <- ifelse(confusion == 0, Inf, confusion)
+    mins <- confusion == min(confusInf)
+    drop <- switch(sum(mins),
+      switch(which(mins),
+             as.logical(splitA & splitB),
+             as.logical(!splitA & !splitB),
+             as.logical(!splitA & splitB),
+             as.logical(splitA & !splitB)),
+      {
+        if (absBal[1] > absBal[2]) {
+          # remove from A, which is less balanced
+          if (balance[1] < 0) {
+            # remove from IN A
+            if (confusion[1] == confusion[4]) {
+              as.logical(splitA)
+            } else if (confusInf[1] < confusInf[4]) {
+              as.logical(splitA & splitB)
             } else {
-              # remove from OUT A
-              if (confusion[2] == confusion[3]) {
-                as.logical(!splitA)
-              } else if (confusInf[2] < confusInf[3]) {
-                as.logical(!splitA & !splitB)
-              } else {
-                as.logical(!splitA & splitB)
-              }
+              as.logical(splitA & !splitB)
             }
           } else {
-            # remove from B, which is less balanced
-            if (balance[2] < 0) {
-              # remove from IN B
-              if (confusion[1] == confusion[3]) {
-                as.logical(splitB)
-              } else if (confusInf[1] < confusInf[3]) {
-                as.logical(splitA & splitB)
-              } else {
-                as.logical(!splitA & splitB)
-              }
+            # remove from OUT A
+            if (confusion[2] == confusion[3]) {
+              as.logical(!splitA)
+            } else if (confusInf[2] < confusInf[3]) {
+              as.logical(!splitA & !splitB)
             } else {
-              # remove from OUT B
-              if (confusion[2] == confusion[4]) {
-                as.logical(!splitB)
-              } else if (confusInf[2] < confusInf[4]) {
-                as.logical(!splitA & !splitB)
-              } else {
-                as.logical(splitA & !splitB)
-              }
+              as.logical(!splitA & splitB)
             }
           }
-        }, 
-        if (abs(Ab - aB) > abs(AB - ab)) {
-          if (aB < Ab) {
-            as.logical(!splitA & splitB)
+        } else {
+          # remove from B, which is less balanced
+          if (balance[2] < 0) {
+            # remove from IN B
+            if (confusion[1] == confusion[3]) {
+              as.logical(splitB)
+            } else if (confusInf[1] < confusInf[3]) {
+              as.logical(splitA & splitB)
+            } else {
+              as.logical(!splitA & splitB)
+            }
           } else {
-            as.logical(splitA & !splitB)
+            # remove from OUT B
+            if (confusion[2] == confusion[4]) {
+              as.logical(!splitB)
+            } else if (confusInf[2] < confusInf[4]) {
+              as.logical(!splitA & !splitB)
+            } else {
+              as.logical(splitA & !splitB)
+            }
           }
+        }
+      }, 
+      if (abs(Ab - aB) > abs(AB - ab)) {
+        if (aB < Ab) {
+          as.logical(!splitA & splitB)
         } else {
-          if (ab < AB) {
-            as.logical(!splitA & !splitB)
-          } else {
-            as.logical(splitA & splitB)
-          } 
-        }, 
-        if (sum(mins) == 4) {
-          1:4 == 1
-        } else {
-          print(confusion)
-          summary(c(splitA, splitB))
-          stop()
-        })
-    } else {
-      
-      disagreementSplit <- xor.Splits(sp[[1]][[i[minMismatch]]],
-                                      sp[[2]][[j[minMismatch]]])
-      drop <- if (TipsInSplits(disagreementSplit, keep.names = FALSE,
-                               smallest = FALSE) != min(mmSize)) {
-        # Divergence from de Oliveira & el, 
-        if (justOne) {
-          which.max(!as.logical(disagreementSplit))
-        } else {
-          !as.logical(disagreementSplit)
+          as.logical(splitA & !splitB)
         }
       } else {
-        if (justOne) {
-          which.max(as.logical(disagreementSplit))
+        if (ab < AB) {
+          as.logical(!splitA & !splitB)
         } else {
-          as.logical(disagreementSplit)
-        }
-      }
+          as.logical(splitA & splitB)
+        } 
+      }, 
+      if (sum(mins) == 4) {
+        1:4 == 1
+      } else {
+        print(confusion)
+        summary(c(splitA, splitB))
+        stop()
+      })
+
+    if (!any(drop)) {
+      splitA <<- splitA
+      splitB <<- splitB
+      stopifnot(any(drop))
     }
-    stopifnot(any(drop))
     if (sum(drop) > 1) {
       drop <- as.logical(tabulate(which.max(drop), length(drop)))
     }
@@ -274,4 +257,55 @@ SPRDist.multiPhylo <- SPRDist.list
   # Return:
   
   if (debug) list(moves, dropList) else moves
+}
+
+# For comparison: not as optimized as phangorn::SPR.dist
+.SPRPairDeO <- function(tree1, tree2) {
+  moves <- 0
+  simplified <- TreeConflict(tree1, tree2)
+  
+  while (!is.null(simplified)) {
+    sp <- as.Splits(simplified)
+    nSplits <- length(sp[[1]])
+    i <- rep(seq_len(nSplits), nSplits)
+    j <- rep(seq_len(nSplits), each = nSplits)
+    mmSize <- mismatch_size(sp[[1]], sp[[2]])
+    minMismatch <- which.min(mmSize)
+    if (mmSize[minMismatch] == 0) {
+      agreement <- as.logical(sp[[1]][[i[minMismatch]]])
+      subtips1 <- agreement
+      subtips1[!subtips1][1] <- TRUE
+      subtips2 <- !agreement
+      subtips2[agreement][1] <- TRUE
+      return(moves +
+               .SPRPair(KeepTipFast(simplified[[1]], subtips1),
+                        KeepTipFast(simplified[[2]], subtips1)) +
+               .SPRPair(KeepTipFast(simplified[[1]], subtips2),
+                        KeepTipFast(simplified[[2]], subtips2))
+             )
+    }
+    
+    disagreementSplit <- xor.Splits(sp[[1]][[i[minMismatch]]],
+                                    sp[[2]][[j[minMismatch]]])
+    drop <- if (TipsInSplits(disagreementSplit, keep.names = FALSE,
+                             smallest = FALSE) != min(mmSize)) {
+      !as.logical(disagreementSplit)
+    } else {
+      as.logical(disagreementSplit)
+    }
+    if (sum(drop) > 1) {
+      drop <- as.logical(tabulate(which.max(drop), length(drop)))
+    }
+    simplified <- DropTip(simplified, drop)
+    simplified <- TreeConflict(
+      root_on_node(simplified[[1]], 1),
+      root_on_node(simplified[[2]], 1),
+      check = FALSE
+    )
+
+    moves <- moves + 1
+  }
+  
+  # Return:
+  moves
 }
