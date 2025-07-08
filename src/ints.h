a1cac06328f929f28aa470aec22bbf547ba39391
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-08 16:15:11 +0100

    →constexpr

--- src/ints.h
+++ src/ints.h
@@ -4,11 +4,12 @@
 #include <cstdint>
 
 typedef int_fast16_t int16;
-#define INT_16_MAX uint16(INT_FAST16_MAX)
 typedef int_fast32_t int32;
 typedef uint_fast16_t uint16;
-#define UINT_16_MAX uint16(UINT_FAST16_MAX)
 typedef std::vector<decltype(NA_INTEGER)> grf_match;
 
-#define NA_INT16 int16(-0x7fff)
+constexpr uint16 INT_16_MAX = INT_FAST16_MAX;
+constexpr uint16 UINT_16_MAX = UINT_FAST16_MAX;
+constexpr int16 NA_INT16 = -0x7fff;
+
 #endif
