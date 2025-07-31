2e5caaa1bb0efea1c9dad33647b66fc12cde8f27
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-07-31 17:17:03 +0100

    non-2 multiple

--- benchmark/bench-LAPJV.R
+++ benchmark/bench-LAPJV.R
@@ -4,7 +4,7 @@ set.seed(1)
 UnifMat <- function(n) matrix(runif(n * n), n, n)
 test40 <- UnifMat(40)
 test400 <- UnifMat(400)
-test2000 <- UnifMat(2000)
+test2000 <- UnifMat(1999) # non-2 multiple
 
 ub(LAPJV(test40), LAPJV(test400), times = 100)
 # With stl vectors in caller only
