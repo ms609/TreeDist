468b34c1da08fcb66a9fa04ade9984b276b34a29
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-05 07:01:47 +0100

    Script: pairing

--- inst/treeKO.R
+++ inst/treeKO.R
@@ -1,5 +1,5 @@
 library("TreeTools")
-geneTree1 <- ape::read.tree(text = "((E, D), (A3, ((C, A2), (B, A1))D2 )D1);")
+geneTree1 <- ape::read.tree(text = "(root, ((E, D), (A3, ((C, A2), (B, A1))D2 )D1));")
 plot(geneTree1, show.node.label = TRUE)
 nodelabels()
 edgelabels()
@@ -27,4 +27,29 @@ DuplicationTrees <- function(tree, duplications) {
     do.call(c, lapply(children, function(ch) DuplicationTrees(DropTip(tree, ch))))
   }
 }
-DuplicationTrees(geneTree1)
+
+DenumberTips <- function(trees) {
+  lapply(trees, function(tr) {
+    tr[["tip.label"]] <- gsub("\\d+$", "", tr[["tip.label"]], perl = TRUE)
+    tr
+  })
+}
+
+dupTrees <- DenumberTips(DuplicationTrees(geneTree1))
+
+par(mfrow = c(1, 3), mar = rep(0, 4))
+for (tr in dupTrees) plot(tr)
+
+# All trees have distance zero - not very interesting for next step
+distMat <- ClusteringInfoDistance(dupTrees)
+
+# Some trees that differ
+dupTrees <- c(as.phylo(0:2, 8), as.phylo(2:4, 6))
+distMat <- as.matrix(ClusteringInfoDistance(dupTrees, normalize = TRUE))
+leavesRemaining <- NTip(dupTrees)
+leavesPruned <- 8 - leavesRemaining
+r <- outer(leavesRemaining, leavesRemaining, `+`)
+p <- outer(leavesPruned, leavesPruned, `+`)
+
+d <- ((distMat * r) + p) / (r + p)
+pairing <- LAPJV(`diag<-`(d, 1))[["matching"]]
