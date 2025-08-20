500db111ae72f4b3ba263bcfb45e04822c0e81a6
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-08-20 19:49:14 +0100

    Modernize benchmarking protocol

--- benchmark/_init.R
+++ benchmark/_init.R
@@ -1,12 +1,22 @@
-library("TreeTools", quietly = TRUE)
-library("TreeDist")
-ub <- microbenchmark::microbenchmark
-set.seed(1337)
+library("TreeTools")
+set.seed(54321)
 
-Benchmark <- function(id, result) {
+Benchmark <- function(..., min_iterations = NULL) {
+  result <- bench::mark(..., min_iterations = min_iterations %||% 3,
+                        time_unit = "us")
   if (interactive()) {
     print(result)
   } else {
-    saveRDS(result, paste0(id, ".bench.Rds"))
+    fileroot <- gsub("[\"']", "",
+                     gsub("[\\(\\):, /]", "_", as.character(result$expression)))
+    .FileName <- function(fileRoot, i) {
+      paste0(c(fileroot, i, "bench.Rds"), collapse = ".")
+    }
+    i <- double(0)
+    while(file.exists(.FileName(fileroot, i))) {
+      if (length(i) == 0) i <- 0
+      i <- 1 + i
+    }
+    saveRDS(result, .FileName(fileroot, i))
   }
 }
