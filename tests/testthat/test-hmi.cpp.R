93e88f922c5d1e16d269ee0855f5d09c4d83447b
RevBayes analysis <1695515+ms609@users.noreply.github.com>
2025-09-06 13:48:06 +0100

    update tests; document

--- tests/testthat/test-hmi.cpp.R
+++ tests/testthat/test-hmi.cpp.R
@@ -3,7 +3,10 @@ library("TreeTools")
 test_that("HMI calculated correctly", {
   bal6 <- BalancedTree(6)
   pec6 <- PectinateTree(6)
+  expect_equal(HierarchicalMutualInfo(bal6, pec6),
+               HierachicalMutual(bal6, pec6))
   expect_equal(d_n_nested(phylo_to_nested(bal6),
                           phylo_to_nested(pec6)),
                HierachicalMutual(bal6, pec6))
+
 })
